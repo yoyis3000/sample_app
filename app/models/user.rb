a205672f5396da0906492f8cc67009c5a62b6901
yoyis3000 <georgebarrios@umail.ucsb.edu>
2014-04-22 16:09:44 -0700

    add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
+  has_many :microposts, dependent: :destroy
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
   validates:name, presence: true, length:  { maximum: 50 }
@@ -20,6 +21,10 @@ class User < ActiveRecord::Base
                    uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+  def feed
+    #This will be changed later, eventually
+    Micropost.where("user_id = ?", id)
+  end
   private
     def create_remember_token
       self.remember_token = SecureRandom.urlsafe_base64
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   #get "users/new"
   resources :users
+  resources :microposts, only: [:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   root to:  'static_pages#home'
   match '/signup',  to: 'users#new'
